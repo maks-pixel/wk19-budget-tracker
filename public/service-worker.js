cdd6f248dbbe585d4f3fcf62685fbb7fca97324d
Makda Girmay <makdagirmay4@gmail.com>
2022-01-27 02:51:02 -0500

    small changes

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,10 @@ const FILES_TO_CACHE = [
     "./index.html",
     "./css/styles.css",
     "./js/idb.js",
-    "./js/index.js"
+    "./js/index.js",
+    "../models/transaction.js",
+    "../routes/api.js",
+    "../server.js"
 ];
 
 // Respond with cached resources
